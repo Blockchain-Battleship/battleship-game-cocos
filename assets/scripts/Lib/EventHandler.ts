bae1956654fdd02ddd2207b5139b7904d050e917
Ebube Ud <kripsonud@gmail.com>
2021-10-25 15:36:37 +0100

    Add Event Handler

---
+++ assets/scripts/Lib/EventHandler.ts
@@ -0,0 +1,23 @@
+export class EventHandler
+{
+    static registry = {};
+
+    publish = function(name: string, ...args: any) {
+        if (!this.registry[name]) return;
+
+        this.registry[name].forEach(x => {
+            x.apply(null, args);
+        });
+    };
+
+
+     subscribe = function(name: string, fn: any) {
+        if (!this.registry[name]) {
+            this.registry[name] = [fn];
+        } else {
+            this.registry[name].push(fn);
+        }
+    };
+}
+
+ 
